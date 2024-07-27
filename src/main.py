43a0ba6d6eec9fd1c6f36f802d27d37ada9616ab
charudatta <152109007c@gmail.com>
2024-07-27 22:35:18 +0530

    major bug fix

--- src/main.py
+++ src/main.py
@@ -14,4 +14,49 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
-import os from crewai import Agent, Task, Crew, Process  ### OLLAMA (THANKS TO LANGCHAIN) from langchain_community.llms import Ollama ollama_model = Ollama(model="gemma:2b")   ### OPENAI # os.environ["OPENAI_API_KEY"] = "Your Key" #export OPENAI_API_KEY=sk-blablabla # on Linux/Mac   # Define your agents with roles and goals researcher = Agent(   role='Researcher',   goal='Discover new insights',   backstory="You're a world class researcher working on a major data science company",   verbose=True,   allow_delegation=False,   llm=ollama_model, ### OLLAMA VERSION!!   # llm=OpenAI(temperature=0.7, model_name="gpt-4"). It uses langchain.chat_models, default is GPT4 ### OPENAI VERSION!! ) writer = Agent(   role='Writer',   goal='Create engaging content',   backstory="You're a famous technical writer, specialized on writing data related content",   verbose=True,   allow_delegation=False,   llm=ollama_model ### OLLAMA VERSION!! )  # Create tasks for your agents task1 = Task(description='Investigate the latest AI trends', expected_output="Create bullet report on latest ai trends", agent=researcher) task2 = Task(description='Write a blog post on AI advancements', expected_output="write a article blog post", agent=writer)  # Instantiate your crew with a sequential process - TWO AGENTS! crew = Crew(   agents=[researcher, writer],   tasks=[task1, task2],   llm=ollama_model, ### OLLAMA VERSION!!   verbose=2, # Crew verbose more will let you know what tasks are being worked on, you can set it to 1 or 2 to different logging levels   process=Process.sequential # Sequential process will have tasks executed one after the other and the outcome of the previous one is passed as extra content into this next. )  # Get your crew to work! result = crew.kickoff()
+
+import os from crewai 
+import Agent, Task, Crew, Process  
+
+from langchain_community.llms import Ollama 
+
+ollama_model = Ollama(model="gemma:2b")   
+
+ 
+# # Define your agents with roles and goals 
+researcher = Agent(   role='Researcher',
+                      goal='Discover new insights',
+                      backstory="You're a world class researcher working on a major data science company",
+                      verbose=True,
+                      allow_delegation=False,  
+                      llm=ollama_model
+                      ) 
+
+writer = Agent(   role='Writer',
+                  goal='Create engaging content',   
+                  backstory="You're a famous technical writer, specialized on writing data related content",   
+                  verbose=True,
+                  allow_delegation=False,
+                  llm=ollama_model
+                  )  
+
+# Create tasks for your agents 
+task1 = Task(description='Investigate the latest AI trends', 
+             expected_output="Create bullet report on latest ai trends", 
+             agent=researcher
+             ) 
+
+task2 = Task(description='Write a blog post on AI advancements', 
+             expected_output="write a article blog post", 
+             agent=writer)  
+
+# Instantiate your crew with a sequential process - TWO AGENTS! 
+crew = Crew(   agents=[researcher, writer],
+               tasks=[task1, task2],   
+               llm=ollama_model,    
+               verbose=2,    
+               process=Process.sequential  
+               )  
+
+# Get your crew to work! 
+result = crew.kickoff()
